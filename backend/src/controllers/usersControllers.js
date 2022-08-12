b92cdcf22a163c23ec53dbe26b8394f7274ed901
unknown <facundo.gabriel.jimenez@gmail.com>
2022-08-12 16:15:07 -0300

    backend: manejo de algunas excepciones en usuarios

--- backend/src/controllers/usersControllers.js
+++ backend/src/controllers/usersControllers.js
@@ -1,48 +1,75 @@
 
 const {getAllUsersService, getUserByIdService, addDashboardToUserService, deleteDashboardFromUserService, updateDashboardFromUserService, searchUserByUsernameService, addUserService} = require("../services/usersServices")
+const {checkIntegerId} = require("../utils/errorHandling")
 
-const getAllUsers = async (req, res) => {
-    const users = await getAllUsersService();
-    res.json(users);
+const getAllUsers = async (req, res, next) => {
+    try{
+        const users = await getAllUsersService();
+        res.json(users);
+    }catch(err){
+        next(err.message)
+    }
 }
 
-const getUserById = async (req, res) => {
-    const id = parseInt(req.params.id); 
-    const user = await getUserByIdService(id);
-    res.json(user);
+const getUserById = async (req, res, next) => {
+    try{
+        const id = parseInt(req.params.id); 
+        checkIntegerId(id)
+        const user = await getUserByIdService(id);
+        res.json(user);
+    }catch(err){
+        next(err.message)
+    }
 }
 
-const addDashboardToUser = async (req, res) => {
-    const userId = parseInt(req.params.id); 
-    const dashboard = req.body;
-    const user = await addDashboardToUserService(userId, dashboard);
-    res.json(user);
+const addDashboardToUser = async (req, res, next) => {
+    try{
+        const userId = parseInt(req.params.id); 
+        const dashboard = req.body;
+        checkIntegerId(userId);
+        const user = await addDashboardToUserService(userId, dashboard);
+        res.json(user);
+    }catch(err){
+        next(err.message)
+    }
 }
 
-const updateDashboardFromUser = async (req, res) => {
-    const userId = parseInt(req.params.id); 
-    const newData = req.body;
-    const user = await updateDashboardFromUserService(userId, newData);
-    res.json(user);
+const updateDashboardFromUser = async (req, res, next) => {
+    try{
+        const userId = parseInt(req.params.id); 
+        const newData = req.body;
+        checkIntegerId(userId)
+        const user = await updateDashboardFromUserService(userId, newData);
+        res.json(user);
+
+    }catch(err){
+        next(err.message)
+    }
 }
 
-const deleteDashboardFromUser = async (req, res) => {
-    const userId = parseInt(req.params.id); 
-    const dashboardId = parseInt(req.query.dashboardId)
-    const user = await deleteDashboardFromUserService(userId, dashboardId);
-    res.json(user);
+const deleteDashboardFromUser = async (req, res, next) => {
+    try{
+        const userId = parseInt(req.params.id); 
+        const dashboardId = parseInt(req.query.dashboardId);
+        checkIntegerId(userId);
+        checkIntegerId(dashboardId);
+        const user = await deleteDashboardFromUserService(userId, dashboardId);
+        res.json(user);
+    }catch(err){
+        next(err.message)
+    }
 }
 
-const searchUserByUsername = async (req, res) => {
-    const username = req.query.username; 
-    const user = await searchUserByUsernameService(username);
-    res.json(user);
+const searchUserByUsername = async (req, res, next) => {
+    try{
+        const username = req.query.username; 
+        const user = await searchUserByUsernameService(username);
+        res.json(user);
+    }catch(err){
+        next(err.message)
+    }
 }
 
-const addUser = async (req, res) => {
-    const user = await addUserService(req.body);
-    res.json(user);
-}
 
 module.exports = {
     getAllUsers,
@@ -50,6 +77,5 @@ module.exports = {
     searchUserByUsername,
     addDashboardToUser,
     deleteDashboardFromUser,
-    updateDashboardFromUser,
-    addUser
+    updateDashboardFromUser
 }
